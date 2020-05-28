a16903dcb61d0435815a2a716a425d0e936f0f1e
Oluwatosin <jafolayan06@gmail.com>
2020-05-28 13:54:31 +0100

    Added authentication db context class

--- src/EHouseApp.Data/DatabaseContexts/AuthenticationDbContext/AuthenticationDbContext.cs
+++ src/EHouseApp.Data/DatabaseContexts/AuthenticationDbContext/AuthenticationDbContext.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿using EHouseApp.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace EHouseApp.Data.DatabaseContexts.AuthenticationDbContext
 {
-    class AuthenticationDbContext
+    class AuthenticationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public AuthenticationDbContext(DbContextOptions<AuthenticationDbContext> options)
+            : base(options)
+        {
+
+        }
     }
 }
