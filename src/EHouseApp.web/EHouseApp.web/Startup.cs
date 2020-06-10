eba2071143576ba478ecb24c868a4cbbb53ef9a3
Oluwatosin <jafolayan06@gmail.com>
2020-06-10 16:35:53 +0100

    added user and roles

--- src/EHouseApp.web/EHouseApp.web/Startup.cs
+++ src/EHouseApp.web/EHouseApp.web/Startup.cs
@@ -1,8 +1,11 @@
 using System;
+using System.Threading.Tasks;
 using EHouseApp.Data.DatabaseContexts.ApplicationDbContext;
 using EHouseApp.Data.DatabaseContexts.AuthenticationDbContext;
+using EHouseApp.Data.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -39,10 +42,25 @@ namespace EHouseApp.web
                 sqlServerOptions.MigrationsAssembly("ehouseapp.data");
                 }
             ));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<AuthenticationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.SignIn.RequireConfirmedEmail = false;
+                options.SignIn.RequireConfirmedPhoneNumber = false;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider svp)
         {
             if (env.IsDevelopment())
             {
@@ -67,6 +85,51 @@ namespace EHouseApp.web
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            MigrateDatabaseContexts(svp);
+            CreateDefaultRolesAndUsers(svp).GetAwaiter().GetResult();
+        }
+
+        public void MigrateDatabaseContexts(IServiceProvider svp)
+        {
+            var authenticationDbContext = svp.GetRequiredService<AuthenticationDbContext>();
+            authenticationDbContext.Database.Migrate();
+
+            var applicationDbContext = svp.GetRequiredService<ApplicationDbContext>();
+            applicationDbContext.Database.Migrate();
+        }
+
+        public async Task CreateDefaultRolesAndUsers(IServiceProvider svp)
+        {
+            string[] roles = new string[] { "SystemAdministrator", "Agent", "User" };
+            var userEmail = "admin@ehouseapp.com";
+            var userPassword = "superpassword@2020";
+
+            var roleManager = svp.GetRequiredService<RoleManager<IdentityRole>>();
+            foreach (var role in roles) 
+            {
+                var roleExist = await roleManager.RoleExistsAsync(role);
+                if (!roleExist)
+                {
+                    await roleManager.CreateAsync(new IdentityRole{Name = role});
+                }
+            }
+
+            var userManager = svp.GetRequiredService<UserManager<ApplicationUser>>();
+            var user = await userManager.FindByEmailAsync(userEmail);
+            if(user is null)
+            {
+                user = new ApplicationUser
+                {
+                    Email = userEmail,
+                    UserName = userEmail,
+                    EmailConfirmed = true,
+                    PhoneNumber = "+23409090087444",
+                    PhoneNumberConfirmed = true
+                };
+                await userManager.CreateAsync(user, userPassword);
+                await userManager.AddToRolesAsync(user, roles);
+            }
         }
     }
 }
