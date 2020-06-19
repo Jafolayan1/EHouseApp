e346b7dc666941161ee7b627b9856119664d872a
Oluwatosin <jafolayan06@gmail.com>
2020-06-19 03:43:13 +0100

    Adde model for Login\Register

--- src/EHouseApp.web/EHouseApp.web/Controllers/AccountController.cs
+++ src/EHouseApp.web/EHouseApp.web/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EHouseApp.web.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,14 +10,32 @@ namespace EHouseApp.web.Controllers
 {
     public class AccountController : Controller
     {
+        [HttpGet]
         public IActionResult Login()
         {
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Login(LoginModel model)
+        {
+            if (!ModelState.IsValid) return View();
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Register(RegisterModel model)
+        {
+            if (!ModelState.IsValid) return View();
+            throw new NotImplementedException();
+            
+        }
+
     }
 }
---
+++ src/EHouseApp.web/EHouseApp.web/Models/RegisterModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EHouseApp.web.Models
+{
+    public class RegisterModel 
+    {
+        [DisplayName("Name")]
+        [Required]
+        public string FullName { get; set; }
+
+        [DisplayName("Email Address")]
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DisplayName("Confirm Password")]
+        [Required]
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
