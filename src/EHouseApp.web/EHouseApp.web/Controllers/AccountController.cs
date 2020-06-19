1c4565ebcee5a0cf5f8a64760b5af5ba9a2f6a97
Oluwatosin <jafolayan06@gmail.com>
2020-06-19 01:16:20 +0100

    Added login UI

---
+++ src/EHouseApp.web/EHouseApp.web/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EHouseApp.web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
