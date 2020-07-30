25806e171ee8fa5399f4c97d19087356e3d231ff
Oluwatosin <jafolayan06@gmail.com>
2020-07-30 10:28:23 +0100

    added columns to property entity

--- src/EHouseApp.Data/Entities/Property.cs
+++ src/EHouseApp.Data/Entities/Property.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 
-
 namespace EHouseApp.Data.Entities
 {
-    public class Property :BaseEntity
+    public class Property : BaseEntity
     {
-        public string Title { get; set; } // 3 Bedroom Flat In Abule Egba.
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public int NumberOfRooms { get; set; }
+        public int NumberOfBaths { get; set; }
+        public int NumberOfToilets { get; set; }
+        public string Address { get; set; }
+        public string ContactPhoneNumber { get; set; }
     }
 }
- 
---
+++ src/EHouseApp.web/EHouseApp.web/Controllers/PropertiesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EHouseApp.web.Controllers
+{
+    public class PropertiesController : Controller
+    {
+        public PropertiesController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
