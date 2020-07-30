2ef59344f6e9988cb5efc646b48ebb8b42622d30
Oluwatosin <jafolayan06@gmail.com>
2020-07-30 17:48:15 +0100

    Can get property details and pass to controller

--- src/EHouseApp.web/EHouseApp.web/Models/PropertyModel.cs
+++ src/EHouseApp.web/EHouseApp.web/Models/PropertyModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,14 +9,36 @@ namespace EHouseApp.web.Models
 {
     public class PropertyModel
     {
+        [Required]
+        [StringLength(maximumLength: 25, MinimumLength = 8)]
         public string Title { get; set; }
+
         public string ImageUrl { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
         public double Price { get; set; }
+
+        [Required]
         public string Description { get; set; }
+
+        [Required]
+        [MaxLength(20, ErrorMessage = "Cannot be more than  20")]
         public int NumberOfRooms { get; set; }
+
+        [Required]
+        [MaxLength(20, ErrorMessage = "Cannot be more than  20")]
         public int NumberOfBaths { get; set; }
+
+        [Required]
+        [MaxLength(20, ErrorMessage = "Cannot be more than  20")]
         public int NumberOfToilets { get; set; }
+
+        [Required]
         public string Address { get; set; }
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
         public string ContactPhoneNumber { get; set; }
     }
 }
---
+++ src/EHouseApp.web/EHouseApp.web/Services/PropertyServices.cs
@@ -0,0 +1,13 @@
+﻿using EHouseApp.web.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EHouseApp.web.Services
+{
+    public class PropertyServices : IPropertyService
+    {
+    }
+}
